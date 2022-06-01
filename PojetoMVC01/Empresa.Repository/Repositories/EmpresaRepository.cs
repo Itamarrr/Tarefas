0a40749cdd491be6836ade31fe9ba435ad3681b6
itamarrr <itamarsantos.dev.oli@gmail.com>
2022-06-01 17:54:50 -0300

    instalado o Daaper e System.Data.SqlClient Criado a query da EmpresaRepository com a propriedade privada connectionstring

--- PojetoMVC01/Empresa.Repository/Repositories/EmpresaRepository.cs
+++ PojetoMVC01/Empresa.Repository/Repositories/EmpresaRepository.cs
@@ -1,43 +1,73 @@
-﻿using Empresa.Domain.Entities;
+﻿using Dapper;
+using Empresa.Domain.Entities;
 using Empresa.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace Empresa.Repository.Repositories
 {
     public class EmpresaRepository : IEmpresaRepository
     {
-
+        //Atributo
+        private string connectionstring;
         public void Inserir(EmpresaEntities obj)
         {
-            throw new NotImplementedException();
+            var query = @"INSERT INTO EMPRESA(IDEMRESA, RAZAOSOCIAL, CNPJ)
+                            VALUES(@IDEMPRESA, @RAZAOSOCIAL, @CNPJ)";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Execute(query, obj);
+            }
+           
         }
 
         public void Alterar(EmpresaEntities obj)
         {
-            throw new NotImplementedException();
+            var query = @"UPDATE EMPRESA SETE RAZAOSOCIAL = @RAZAOSOCIAL
+                          WHERE IDEMPRESA = @IDEMPRESA";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Execute(query, obj);
+            }
         }
 
         public void Exluir(EmpresaEntities obj)
         {
-            throw new NotImplementedException();
+            var query = @"DELETE FROM EMPRESA WHERE IDEMPRESA = @IDEMPRESA";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Execute(query, obj);
+            }
         }
-       
-
+        public List<EmpresaEntities> ObterTodos()
+        {
+            var query = @"SELECT * FROM EMPRESA ORDER BY RAZAOSOCIAL ";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                return connection.Query<EmpresaEntities>(query).ToList();
+            }
+        }
         public EmpresaEntities ObterPorId(Guid id)
         {
-            throw new NotImplementedException();
+            var query = @"SELECT * FROM EMPRESA WHERE IDEMPRESA = @id";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                return connection.Query<EmpresaEntities>(query, new {id}).FirstOrDefault();
+            }
         }
 
         public EmpresaEntities ObterPorCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            var query = @"SELECT * FROM WHERE CNPJ = @cnpj";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                return connection.Query<EmpresaEntities>(query, new {cnpj}).FirstOrDefault();
+            }
         }
 
-        public List<EmpresaEntities> ObterTodos()
-        {
-            throw new NotImplementedException();
-        }
+      
     }
 }
